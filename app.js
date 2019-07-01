f6cf433ee85c422d25e17a6f55d50765f9884e04
XLLU <lucasluu@yahoo.com>
2019-07-01 21:02:47 +0800

    images

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 //app.js
 App({
   
+  globalData: {
+
+  },
+
+	onLaunch() {
+    wx.request({
+      url: 'https://raw.githubusercontent.com/XLLU/mini-mobile-case/master/assets/db.json',
+      success: (response) => {
+        Object.assign(this.globalData,response.data)
+      }
+    })
+  }
+
 })
