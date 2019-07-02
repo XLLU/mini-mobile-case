56485ad25246bed04b117f920f306f4b5f40eda4
XLLU <lucasluu@yahoo.com>
2019-07-02 14:19:03 +0800

    Slides Done

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ App({
       url: 'https://raw.githubusercontent.com/XLLU/mini-mobile-case/master/assets/db.json',
       success: (response) => {
         Object.assign(this.globalData,response.data)
+
+        const currentPages = getCurrentPages();
+        if (currentPages.length !== 0) {
+          currentPages[currentPages.length-1].onload()
+        }
       }
     })
   }
